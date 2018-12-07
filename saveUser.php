091339ce2877f2eab1ef77379b823704273989c0
Cicada1105 <guitarlegen@gmail.com>
2018-12-07 11:16:55 -0800

    Added semicolon

--- saveUser.php
+++ saveUser.php
@@ -1,7 +1,7 @@
 <?php
 	$response = $_RESPONSE["data"];
 	$file = $fopen("user.json","w") or die("Was unable to open file");
-	$fwrite($response,$filesize($file))
+	$fwrite($response,$filesize($file));
 	$fclose();
 	echo "Saved";
 ?>
